6b5e8c0c4c7b9bcb6bb8de5fb41d8aa9042a0006
Katlego Maredi <katlegomaredi9@gmail.com>
2024-02-27 22:50:53 +0200

    These are my chapter 4 exercises

---
+++ Chapter_04/exercise4_1.js
@@ -0,0 +1,13 @@
+//Practice exercise 4.1
+let isCold = true;
+//Output the value to the console
+console.log(isCold);
+//Check if the value is true
+if(isCold === true)
+{
+    console.log(isCold);
+}
+else
+{
+    console.log("It is not cold");
+}
---
+++ Chapter_04/exercise4_3.js
@@ -0,0 +1,7 @@
+//Pratice exercise 4.3
+//ID variable
+let id = false;
+//Use a tenery operator to check ID is valid
+let access = id === true ? "Allowed" : "Not allwed";
+//Output to console
+console.log(access);
