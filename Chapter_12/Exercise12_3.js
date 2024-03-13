296b3d10fd728338fbb113a2db7db8c874bab527
Katlego Maredi <katlegomaredi9@gmail.com>
2024-03-13 18:45:41 +0200

    Pushing exercise 12.3 code

---
+++ Chapter_12/Exercise12_3.js
@@ -0,0 +1,17 @@
+"use strict";
+
+//A function without parameters
+function loopArgs() {
+    //A variable with a blamk value
+    let lastOne = "";
+    //Creating a loop to iterate through the argumenst
+    for (let i = 0; i < arguments.length; i++) {
+        //As we loop through the array, set the value to in
+        lastOne = arguments[i];
+    }
+    //Returning last one value
+    return lastOne;
+}
+
+//Output to the console
+console.log(loopArgs("Value1", "TWO", "Three", "Four", "Five", "Six", "Oi"));
